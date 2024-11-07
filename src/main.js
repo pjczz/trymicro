3ee05ac8078d46b8561cb72073a892412dd7e91c
pjczz <1435211553@qq.com>
2024-11-07 15:53:01 +0800

    🎉 init:

--- src/main.js
+++ src/main.js
@@ -2,5 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import microApp from '@micro-zoe/micro-app'
 
 createApp(App).use(store).use(router).mount('#app')
+microApp.start()
